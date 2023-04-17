ef66d1af37d732934ad57b044cabeeefc5b841e4
khamudom <khamu@outlook.com>
2023-04-16 22:26:22 -0700

    added images and more styling work

--- src/App.tsx
+++ src/App.tsx
@@ -1,124 +1,207 @@
 import './App.css';
 import { HiBell, HiCog6Tooth, HiShoppingCart } from 'react-icons/hi2';
-import { catalogNewReleases } from './data';
+import {
+  FaDiscord,
+  FaFacebookF,
+  FaSearch,
+  FaSlackHash,
+  FaTwitter,
+} from 'react-icons/fa';
+import {
+  catalogFamilyFriendly,
+  catalogNewReleases,
+  catalogAction,
+  catalogTopSelling,
+  heroItem1,
+  heroItem2,
+} from './data';
 
 function App() {
   return (
     <div className="container">
-      <header>
-        <nav>
-          <div className="logo">
-            <img src="/images/astronaut.jpg" alt="store logo" />
+      <div className="wrapper">
+        <header>
+          <nav>
+            <div className="logo">
+              <img src="/images/astronaut.jpg" alt="store logo" />
+            </div>
+            <ul>
+              <li>Home</li>
+              <li>Deals</li>
+              <li>Staff Picks</li>
+              <li>Reviews</li>
+            </ul>
+          </nav>
+          <div className="account">
+            <HiBell className="icon" />
+            <HiShoppingCart className="icon" />
+            <HiCog6Tooth className="icon" />
           </div>
+        </header>
+        <main>
+          <section className="filter-section">
+            <div className="input-wrapper">
+              <input type="search" placeholder="Search" />
+              <FaSearch className="icon" />
+            </div>
+            <div className="divider"></div>
+          </section>
+          <section className="hero-section">
+            {heroItem1.map((item) => {
+              return (
+                <div className="hero-card" key={item.id}>
+                  <img src={item.image} alt={item.title} />
+                  <div className="info">
+                    <div className="header">
+                      <h3 className="title">{item.title}</h3>
+                      <p>{item.description}</p>
+                    </div>
+                    <div className="footer">
+                      <button>Detail</button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          <section className="catalog">
+            <div className="catalog-row">
+              <h3 className="catalog-header">New Releases</h3>
+              <ul>
+                {catalogNewReleases.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <div className="card">
+                        <img
+                          className="box-image"
+                          src={item.image}
+                          alt={item.title}
+                        />
+                        <p className="title">{item.title}</p>
+                        <p className="developer">{item.developer}</p>
+                        <p className="price">{item.price}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+          <section className="catalog">
+            <div className="catalog-row">
+              <h3 className="catalog-header">Top Selling</h3>
+              <ul>
+                {catalogTopSelling.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <div className="card">
+                        <img
+                          className="box-image"
+                          src={item.image}
+                          alt={item.title}
+                        />
+                        <p className="title">{item.title}</p>
+                        <p className="developer">{item.developer}</p>
+                        <p className="price">{item.price}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+          <section className="hero-section">
+            {heroItem2.map((item) => {
+              return (
+                <div className="hero-card" key={item.id}>
+                  <img src={item.image} alt={item.title} />
+                  <div className="info">
+                    <div className="header">
+                      <h3 className="title">{item.title}</h3>
+                      <p>{item.description}</p>
+                    </div>
+                    <div className="footer">
+                      <button>Detail</button>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          <section className="catalog">
+            <div className="catalog-row">
+              <h3 className="catalog-header">Action</h3>
+              <ul>
+                {catalogAction.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <div className="card">
+                        <img
+                          className="box-image"
+                          src={item.image}
+                          alt={item.title}
+                        />
+                        <p className="title">{item.title}</p>
+                        <p className="developer">{item.developer}</p>
+                        <p className="price">{item.price}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+          <section className="product-section">
+            <div className="product-card"></div>
+            <div className="product-card"></div>
+          </section>
+          <section className="catalog">
+            <div className="catalog-row">
+              <h3 className="catalog-header">Family Friendly</h3>
+              <ul>
+                {catalogFamilyFriendly.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <div className="card">
+                        <img
+                          className="box-image"
+                          src={item.image}
+                          alt={item.title}
+                        />
+                        <p className="title">{item.title}</p>
+                        <p className="developer">{item.developer}</p>
+                        <p className="price">{item.price}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+        </main>
+        <footer>
           <ul>
             <li>Home</li>
             <li>Deals</li>
             <li>Staff Picks</li>
             <li>Reviews</li>
           </ul>
-        </nav>
-        <div className="account">
-          <HiBell className="icon" />
-          <HiShoppingCart className="icon" />
-          <HiCog6Tooth className="icon" />
-        </div>
-      </header>
-      <main>
-        <section className="filter">Catalog filter</section>
-        <section className="hero">Hero Section</section>
-        <section className="catalog">
-          <div className="catalog-row">
-            <h3>New Releases</h3>
-            <ul>
-              {catalogNewReleases.map((item) => {
-                return (
-                  <li key={item.id}>
-                    <div className="card">
-                      <img src={item.image} alt={item.title} />
-                      <p>{item.title}</p>
-                      <p>{item.developer}</p>
-                      <p>{item.price}</p>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
+          <div className="social">
+            <span>
+              <FaTwitter className="icon" />
+            </span>
+            <span>
+              <FaFacebookF className="icon" />
+            </span>
+            <span>
+              <FaDiscord className="icon" />
+            </span>
+            <span>
+              <FaSlackHash className="icon" />
+            </span>
           </div>
-        </section>
-        <section className="catalog">
-          <div className="catalog-row">
-            <h3>Top Selling</h3>
-            <ul>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className="hero">Hero Section</section>
-        <section className="catalog">
-          <div className="catalog-row">
-            <h3>Shooters</h3>
-            <ul>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <section className="hero">Hardware</section>
-        <section className="catalog">
-          <div className="catalog-row">
-            <h3>Family Friendly</h3>
-            <ul>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-              <li>
-                <div className="card"></div>
-              </li>
-            </ul>
-          </div>
-        </section>
-      </main>
-      <footer>
-        <ul>
-          <li>Home</li>
-          <li>Deals</li>
-          <li>Staff Picks</li>
-          <li>Reviews</li>
-        </ul>
-        <div className="social">
-          <span>Twitter</span>
-          <span>Facebook</span>
-          <span>Discord</span>
-          <span>Slack</span>
-        </div>
-      </footer>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/data.ts
+++ src/data.ts
@@ -1,37 +1,179 @@
-export const catalogNewReleases = [
+interface ICatalog {
+  id: number;
+  title: string;
+  image: string;
+  developer: string;
+  price: string;
+}
+
+export const catalogNewReleases: ICatalog[] = [
   {
     id: 0,
-    title: 'game 1',
-    image: 'https://placehold.co/600x400',
-    developer: 'Game Maker',
-    price: 60.0,
+    title: 'Dirt Rally',
+    image: './src/assets/images/newrelease/dirtrally.jpg',
+    developer: 'Codemaster',
+    price: '$60.00',
   },
   {
     id: 1,
-    title: 'game 2',
-    image: 'https://placehold.co/600x400',
-    developer: 'Game Maker',
-    price: 60.0,
+    title: 'Battlefield 5',
+    image: './src/assets/images/newrelease/battlefield.jpg',
+    developer: 'Electronic Arts',
+    price: '$60.00',
   },
   {
     id: 2,
-    title: 'game 3',
-    image: 'https://placehold.co/600x400',
-    developer: 'Game Maker',
-    price: 60.0,
+    title: 'Need For Speed Heat',
+    image: './src/assets/images/newrelease/nfs.jpg',
+    developer: 'Electronic Arts',
+    price: '$60.00',
   },
   {
     id: 3,
-    title: 'game 4',
-    image: 'https://placehold.co/600x400',
-    developer: 'Game Maker',
-    price: 60.0,
+    title: 'Call of Duty: Modern Warfare',
+    image: './src/assets/images/newrelease/callofduty.jpg',
+    developer: 'Activision',
+    price: '$60.00',
   },
   {
     id: 4,
-    title: 'game 5',
-    image: 'https://placehold.co/600x400',
-    developer: 'Game Maker',
-    price: 60.0,
+    title: 'FIFA 20',
+    image: './src/assets/images/newrelease/fifa.jpg',
+    developer: 'Electronic Arts',
+    price: '$60.00',
+  },
+];
+
+export const catalogTopSelling: ICatalog[] = [
+  {
+    id: 0,
+    title: 'Grand Theft Auto 5',
+    image: './src/assets/images/topselling/grandtheft5.jpg',
+    developer: 'Rockstar Games',
+    price: '$60.00',
+  },
+  {
+    id: 1,
+    title: 'Need For Speed',
+    image: './src/assets/images/topselling/nfs1.png',
+    developer: 'Electronic Arts',
+    price: '$60.00',
+  },
+  {
+    id: 2,
+    title: 'Frostpunk',
+    image: './src/assets/images/topselling/frostpunk.jpg',
+    developer: '11 Bit studios',
+    price: '$60.00',
+  },
+  {
+    id: 3,
+    title: 'Borderlands 3',
+    image: './src/assets/images/topselling/borderland3.png',
+    developer: 'Gearbox Software',
+    price: '$60.00',
+  },
+  {
+    id: 4,
+    title: 'Battlefield 1',
+    image: './src/assets/images/topselling/battlefield1.jpg',
+    developer: 'Electronic Arts',
+    price: '$60.00',
+  },
+];
+
+export const catalogAction: ICatalog[] = [
+  {
+    id: 0,
+    title: 'Call of Duty: WW2',
+    image: './src/assets/images/action/callofdutyww2.jpg',
+    developer: 'Activision',
+    price: '$60.00',
+  },
+  {
+    id: 1,
+    title: 'Gears of War 4',
+    image: './src/assets/images/action/gears.jpg',
+    developer: 'Microsoft Studios',
+    price: '$60.00',
+  },
+  {
+    id: 2,
+    title: 'Rise of the Tomb Raider',
+    image: './src/assets/images/action/tomb.png',
+    developer: 'Microsoft Studios',
+    price: '$60.00',
+  },
+  {
+    id: 3,
+    title: 'Star Wars Battlefront',
+    image: './src/assets/images/action/swbattlefront.jpg',
+    developer: 'Electronic Arts',
+    price: '$60.00',
+  },
+  {
+    id: 4,
+    title: 'Prey',
+    image: './src/assets/images/action/prey.jpg',
+    developer: 'Bethesda Softworks',
+    price: '$60.00',
+  },
+];
+
+export const catalogFamilyFriendly: ICatalog[] = [
+  {
+    id: 0,
+    title: 'Minecraft',
+    image: './src/assets/images/family/minecraft.png',
+    developer: 'Mojang Studios',
+    price: '$60.00',
+  },
+  {
+    id: 1,
+    title: 'Lego Harry Potter: Years 1-4',
+    image: './src/assets/images/family/harrypotter.jpg',
+    developer: 'Warner Bros. Games',
+    price: '$60.00',
+  },
+  {
+    id: 2,
+    title: 'Lucky Tales',
+    image: './src/assets/images/family/luckytale.png',
+    developer: 'Playful Studios',
+    price: '$60.00',
+  },
+  {
+    id: 3,
+    title: 'Star Wars',
+    image: './src/assets/images/family/swlego.jpg',
+    developer: 'Warner Bros. Games',
+    price: '$60.00',
+  },
+  {
+    id: 4,
+    title: 'Just Dance',
+    image: './src/assets/images/family/justdance.png',
+    developer: 'Ubisoft',
+    price: '$60.00',
+  },
+];
+
+export const heroItem1 = [
+  {
+    id: 0,
+    title: 'Star Wars Jedi: Fallen Order',
+    description:
+      'An abandoned Padawan must complete his training, develop new powerful Force abilities, and master the art of the lightsaber - all while staying one step ahead of the Empire.',
+    image: './src/assets/images/hero/swfallenorder.jpg',
+  },
+];
+
+export const heroItem2 = [
+  {
+    id: 1,
+    title: 'Destiny 2',
+    description:
+      'Destiny 2 is an action MMO with a single evolving world that you and your friends can join anytime, anywhere, absolutely free.',
+    image: './src/assets/images/hero/destiny.jpg',
   },
 ];
